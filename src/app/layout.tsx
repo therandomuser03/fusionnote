a47a865bd61e0c44142b7a2aa6b616f97d19f1b6
Anubhab <anubhabpal1@gmail.com>
2025-04-04 23:06:46 +0530

    rebuild project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider"
+import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "FusionNote",
-  description: "Your Next smart note taking app",
 };
 
 export default function RootLayout({
@@ -23,11 +14,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex min-h-screen w-full flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">{children}</main>
+            </div>
+            <Toaster />
+          </ThemeProvider>
       </body>
     </html>
   );
